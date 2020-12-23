1817e57aad5a174b59cdf06404e609c68f3bac3b
Lisandro <47686025+Slisandro@users.noreply.github.com>
2020-12-23 19:18:26 -0300

    Update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -14,7 +14,7 @@ function App() {
   function onSearch(ciudad) {
     //Llamado a la API
     fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=4ae2636d8dfbdc3044bede63951a019b`
+      `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=4ae2636d8dfbdc3044bede63951a019b`
     )
       .then((r) => r.json())
       .then((recurso) => {
