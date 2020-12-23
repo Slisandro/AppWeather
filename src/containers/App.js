087a0a0ecbc1ad58e1d15dcfaa636943e7237818
Lisandro <47686025+Slisandro@users.noreply.github.com>
2020-12-23 19:17:05 -0300

    Update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,8 +6,6 @@ import Cards from "../components/Cards.jsx";
 import About from "../components/About.jsx";
 import Ciudad from "../components/Ciudad.jsx";
 
-const apiKey = "4ae2636d8dfbdc3044bede63951a019b";
-
 function App() {
   const [cities, setCities] = useState([]);
   function onClose(id) {
@@ -16,7 +14,7 @@ function App() {
   function onSearch(ciudad) {
     //Llamado a la API
     fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}`
+      `http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=4ae2636d8dfbdc3044bede63951a019b`
     )
       .then((r) => r.json())
       .then((recurso) => {
